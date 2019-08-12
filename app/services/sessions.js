b57b89cce3fb2bbe75d271d70ac016ccf9395f7e
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2019-08-12 22:00:27 +0200

    Program

--- app/services/sessions.js
+++ app/services/sessions.js
@@ -1,5 +1,5 @@
 //import sessions from '../sessions.json';
-const baseUrl = 'https://sleepingpill.javazone.no/public/allSessions/javazone_2018';
+const baseUrl = 'https://api.trondheimdc.no/public/allSessions/TDC2019';
 //const baseUrl = `${sessions}`;
 
 export function getAllSessions() {
