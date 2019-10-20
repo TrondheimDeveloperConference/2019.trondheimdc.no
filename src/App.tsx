5815c9ba891f7211cbfff31be95a45022048af3a
Marvin Bredal Lillehaug <marvin.lillehaug@gmail.com>
2019-10-20 12:49:53 +0200

    Navigation button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from 'react-router-dom';
 
 import tdc_menu from './media/img/tdc_menu.png';
@@ -17,21 +17,28 @@ import Startups from "./Startups";
 import CoC from "./CodeOfConduct";
 import Program from "./program/Program";
 import Session from './program/Session';
+import NavbarToggle from 'react-bootstrap/NavbarToggle'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from "react-bootstrap/Nav";
 
 const App: React.FC = () => {
-  return (
-      <Router>
-        <nav className="navbar sticky-top navbar-opacity navbar-expand-lg navbar-dark text-center mx-auto py-3">
-          <a className="navbar-brand d-block d-sm-none" href="/"><img src={tdc_menu} alt=''/></a>
-          <button className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarNav"
-                  aria-controls="navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+    return (
+        <Router>
+            <Navbar expand='lg' sticky='top' variant='dark' >
+                <Navbar.Brand href="/" className='d-block d-sm-none' />
+                <NavbarToggle label='Toggle navigation' />
+
+                <Navbar.Collapse id="navbarNav" className='mx-auto justify-content-md-center' >
+                    <Nav className="tdc-nav" as='ul'>
+                        <li className="nav-item align-self-center mx-4"><Nav.Link href="/program">program</Nav.Link></li>
+                        <li className="nav-item align-self-center mx-4"><Nav.Link href="/partners">partners</Nav.Link></li>
+                        <li className="nav-item align-self-center mx-4"><Nav.Link href="/" ><img src={tdc_menu} alt=''/></Nav.Link></li>
+                        <li className="nav-item align-self-center mx-4"><Nav.Link href="/startups">startups</Nav.Link></li>
+                        <li className="nav-item align-self-center mx-4"><Nav.Link href="/conduct">code of conduct</Nav.Link></li>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            {/*
 
           <div className="collapse navbar-collapse mx-auto justify-content-md-center" id="navbarNav">
             <ul className="navbar-nav tdc-nav">
@@ -42,51 +49,51 @@ const App: React.FC = () => {
                 <li className="nav-item align-self-center mx-4"><Link className="nav-link" to="/conduct">code of conduct</Link></li>
             </ul>
           </div>
-        </nav>
+        </nav>*/}
 
-        <Switch>
-          <Route exact path="/">
-            <Root />
-          </Route>
-          <Route exact path="/program">
-            <Program />
-          </Route>
-          <Route exact path="/program/:sessionId">
-            <Session />
-          </Route>
-          <Route exact path="/partners">
-            <Partners />
-          </Route>
-          <Route exact path="/startups">
-            <Startups />
-          </Route>
-          <Route exact path="/conduct">
-            <CoC />
-          </Route>
-        </Switch>
+            <Switch>
+                <Route exact path="/">
+                    <Root />
+                </Route>
+                <Route exact path="/program">
+                    <Program />
+                </Route>
+                <Route exact path="/program/:sessionId">
+                    <Session />
+                </Route>
+                <Route exact path="/partners">
+                    <Partners />
+                </Route>
+                <Route exact path="/startups">
+                    <Startups />
+                </Route>
+                <Route exact path="/conduct">
+                    <CoC />
+                </Route>
+            </Switch>
 
-        <footer className="footer">
-          <div className="footer-darkgreen"></div>
-          <div className="footer-green"></div>
-          <div className="container-fluid footer-lightgreen">
-            <div className="d-md-flex flex-md-equal">
-              <div className="overflow-hidden text-center">
-                <img src={tdc_grayscale} alt=''/>
-              </div>
-              <div className="overflow-hidden text-center my-auto text-dark mt-4">
-                <a href="https://www.facebook.com/TDConf"><img src={facebook} alt="Facebook"
-                                                               className="img-fluid mx-2" width="5%"/></a>
-                <a href="https://twitter.com/TrondheimDC"><img src={twitter} alt="Twitter"
-                                                               className="img-fluid mx-2" width="5%"/></a>
-                <a href="mailto:connect@trondheimdc.no"><img src={email} alt="Email"
-                                                             className="img-fluid mx-2" width="5%"/></a>
+            <footer className="footer">
+                <div className="footer-darkgreen"></div>
+                <div className="footer-green"></div>
+                <div className="container-fluid footer-lightgreen">
+                    <div className="d-md-flex flex-md-equal">
+                        <div className="overflow-hidden text-center">
+                            <img src={tdc_grayscale} alt=''/>
+                        </div>
+                        <div className="overflow-hidden text-center my-auto text-dark mt-4">
+                            <a href="https://www.facebook.com/TDConf"><img src={facebook} alt="Facebook"
+                                                                           className="img-fluid mx-2" width="5%"/></a>
+                            <a href="https://twitter.com/TrondheimDC"><img src={twitter} alt="Twitter"
+                                                                           className="img-fluid mx-2" width="5%"/></a>
+                            <a href="mailto:connect@trondheimdc.no"><img src={email} alt="Email"
+                                                                         className="img-fluid mx-2" width="5%"/></a>
 
-              </div>
-            </div>
-          </div>
-        </footer>
-      </Router>
-  );
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </Router>
+    );
 };
 
 export default App;
